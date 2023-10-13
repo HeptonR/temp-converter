61311d003ac64329923a9cae139664434ebd8ba6
HeptonR <russ.hepton@gmail.com>
2023-10-13 18:56:39 +0100

    Dark mode and cleanup

--- kelvin-converter/js/kelvin-converter.js
+++ kelvin-converter/js/kelvin-converter.js
@@ -1,10 +1,19 @@
-function updateVariable() {
+function updateVariable(event) {
+    // Get the value from the Kelvin input
     const kelvin = document.getElementById('kelvinInput');
-    const newValue = parseFloat(kelvin.value);
+    const kelvinInput = parseFloat(kelvin.value);
 
-    const celsius = newValue - 273;
+    // Calculate Kelvin from Celsius and then to Fahrenheit and round off
+    const celsius = kelvinInput - 273;
     const fahrenheit = celsius * (9/5) + 32;
     const roundedFahrenheit = Math.round(fahrenheit);
     
+    // Display the result in the front end
     document.getElementById("result").innerHTML = roundedFahrenheit + '°F';
+
+    // Check and alert if the field is empty
+    if (isNaN(kelvinInput)) {
+        numericValue = kelvinInput;
+        alert("Please enter a temperature in °K.");
+      } 
 }
